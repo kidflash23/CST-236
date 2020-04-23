6e369331c38e0f198b87fae994b50d77527abde4
Ray Omoregie <42901966+kidflash23@users.noreply.github.com>
2020-04-23 16:00:27 -0700

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,5 +1,6 @@
 <?php 
 //This is my first given program for the Activity 1
+//This is another additional comment which I made testing out the file.
 
 
 echo "Hello World. My name is Ray and is this my first PHP program created for the CST-236 course!;"
